8a1e1cb422d5f0f16d0b645fc4a1b73aaee090da
Michael Ruocco <michael.ruocco@hotmail.com>
2021-10-06 08:36:00 +0100

    configures spotless for groovy

--- src/main/groovy/uk/co/mruoc/plugin/environment/properties/GenerateEnvironmentPropertiesTask.groovy
+++ src/main/groovy/uk/co/mruoc/plugin/environment/properties/GenerateEnvironmentPropertiesTask.groovy
@@ -2,8 +2,8 @@ package uk.co.mruoc.plugin.environment.properties
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.TaskAction
 
 class GenerateEnvironmentPropertiesTask extends DefaultTask {
 
